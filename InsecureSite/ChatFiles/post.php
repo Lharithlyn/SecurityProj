42fbd970c62763ac8454fdf6dece4896a996544a
e-s-dewitt <39222670+e-s-dewitt@users.noreply.github.com>
2018-11-14 21:23:20 -0500

    Update post.php

--- InsecureSite/ChatFiles/post.php
+++ InsecureSite/ChatFiles/post.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE php>
+<------Post Prior Chat Function-------->
+<?php
+  session_start();
+  if(isset($_SESSION['name'])){
+    $text = $_POST['text']; 
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+    fclose($fp);
+  }
+?>
