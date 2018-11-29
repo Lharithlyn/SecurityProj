5c9952f85b8b8ba04865ed490277124b295baa95
natehy <nhymer@gmail.com>
2018-11-29 11:29:12 -0500

    Create TextbookInsecureScript.js

---
+++ InsecureSite/TextbookInsecureScript.js
@@ -0,0 +1,9 @@
+	
+	function createCookie(){
+        var creditCard = document.getElementById("CreditCard").value;
+    
+		document.cookie= "CreditCard="+creditCard;
+		//document.write("CreditCard: " +"CC=" + creditCard);
+		//alert(document.cookie);	
+	}
+	
